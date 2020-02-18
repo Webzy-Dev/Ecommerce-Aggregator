2d15546c24685cb240a9b479f2da072edeee861e
Dhaiwat10 <dhaiwatpandya@gmail.com>
2020-02-18 19:09:43 +0530

    Add find order view

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import ListingDisplay from './containers/ListingDisplay/ListingDisplay';
 import CreateListing from './containers/CreateListing/CreateListing';
 import EditListing from './containers/EditListing/EditListing';
 
+import FindOrder from './containers/FindOrder/FindOrder';
+
 function App() {
     return (
         <Router history={history}>
@@ -21,11 +23,16 @@ function App() {
                     <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="mr-auto">
-                            <Nav.Link href="/find-listing">Find Listing</Nav.Link>
-                            <Nav.Link href="#pricing">Pricing</Nav.Link>
                             <NavDropdown title="Listings" id="collasible-nav-dropdown">
                                 <NavDropdown.Item href="/create-listing">Create a new listing</NavDropdown.Item>
                                 <NavDropdown.Item href="/find-listing">View all listings</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider/>
+                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            </NavDropdown>
+                            <NavDropdown title="Orders" id="collasible-nav-dropdown">
+                                <NavDropdown.Item href="/create-listing">Create a new listing</NavDropdown.Item>
+                                <NavDropdown.Item href="/find-order">View all orders</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                                 <NavDropdown.Divider/>
                                 <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
@@ -43,6 +50,9 @@ function App() {
                     </Route>
                     <Route path="/edit-listing/:skuId">
                         <EditListing />
+                    </Route>
+                    <Route path="/find-order">
+                        <FindOrder/>
                     </Route>
                     <Route path="/listing/:id">
                         <ListingDisplay/>
---
+++ src/containers/FindOrder/FindOrder.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react';
+
+import {Link} from 'react-router-dom';
+
+import {Container, Table} from 'react-bootstrap';
+import axios from 'axios';
+
+class FindOrder extends Component {
+
+    state = {
+        loadedOrders: []
+    }
+
+    componentDidMount() {
+        axios
+            .get('http://localhost:8000/orders/all')
+            .then(res => {
+                this.setState({
+                    loadedOrders: {
+                        ...res.data.data
+                    }
+                });
+                console.log(this.state.loadedOrders);
+            })
+            .catch(err => console.log(err));
+    }
+
+    render() {
+        const data = []
+
+        for (let entry in this.state.loadedOrders) {
+            data.push(
+                <tr key={parseInt(entry)}>
+                    <td>{parseInt(entry) + 1}</td>
+                    <td>{this.state.loadedOrders[entry]['order_id']}</td>
+                    <td>{this.state.loadedOrders[entry]['listing_id']}</td>
+                    <td>{this.state.loadedOrders[entry]['shipment_id']}</td>
+                    <td>{this.state.loadedOrders[entry]['status']}</td>
+                    <td><Link to={`/listing/${this.state.loadedOrders[entry]['sku_id']}`}>Open</Link></td>
+                    <td><Link to={`/edit-listing/${this.state.loadedOrders[entry]['sku_id']}`}>Edit</Link></td>
+                </tr>
+            )
+        }
+
+        return (
+            <Container>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Order ID</th>
+                            <th>Listing ID</th>
+                            <th>Shipment ID</th>
+                            <th>Order status</th>
+                            <th>View details</th>
+                            <th>Edit order</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data}
+                    </tbody>
+                </Table>
+            </Container>
+        )
+    }
+}
+
+export default FindOrder;
